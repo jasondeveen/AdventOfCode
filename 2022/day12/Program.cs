3e382923108b379c35a88bc6217ff99533ec93e6
= <=>
2023-10-28 01:26:29 +0200

    another day another dollar

--- 2022/day12/Program.cs
+++ 2022/day12/Program.cs
@@ -1,7 +1,4 @@
-﻿
-using Microsoft.VisualBasic;
-
-namespace day12;
+﻿namespace day12;
 
 class Program
 {
@@ -9,7 +6,8 @@ class Program
     // 2. Put every node in the Unvisited set, and assign value infinity to their cost
     // 3. Assign 0 to the cost of the starting node, and remove it from the Unvisited set, and put it in the Visited set
     // 4. For every node N1 in the Visited set: 
-    //      For every node reachable from this node N2, which is in the Unvisited set, assign the cost value of the node N1 + 1 to the cost of node N2, and remove N2 from the unvisited set and add it to visited
+    //      For every node reachable from this node N2, which is in the Unvisited set, assign the cost value of the node N1 + 1 to the cost of node N2, 
+    //          and remove N2 from the unvisited set and add it to visited
     // 5. Repeat 4. until all node are visited
     // 6. Backtrack from the ending node, taking only steps that allow you to go in the opposite directions, to the starting node. The path followed is the shortest path between S and E
 
@@ -41,45 +39,93 @@ class Program
         Node startNode = graph[startCoor.Item1, startCoor.Item2];
         Node endNode = graph[endCoor.Item1, endCoor.Item2];
 
+        // 1
         foreach(Node n in graph)
         {
             GenerateVertices(n, graph);
         }
 
-        PrintGraph(graph);
+        // PrintGraph(graph);
 
-        System.Console.ReadKey();
+        // 2
+        List<Node> allnodes = graph.Cast<Node>().ToList();
+        List<Node> unvisited = allnodes.ToList();
+
+        // 3
+        List<Node> visited = new();
+        startNode.Cost = 0;
+        unvisited.Remove(startNode);
+
+        int cost = 1;
+
+        // 5
+        while(unvisited.Count > 0)
+        {
+            // 4
+            foreach()
+            {
+
+            }
+        }
+    }
+
+    private static List<Node> GetReachableNeighbours(Node node, Node[,] graph)
+    {
+        List<Node> returnValue = new();
+
+        if(node.Up)
+            returnValue.Add(graph[node.Row - 1, node.Column]);
+
+        if(node.Down)
+            returnValue.Add(graph[node.Row + 1, node.Column]);
+
+        if (node.Left)
+            returnValue.Add(graph[node.Row, node.Column - 1]);
+
+        if (node.Right)
+            returnValue.Add(graph[node.Row, node.Column + 1]);
+
+        return returnValue;
     }
 
     private static void PrintGraph(Node[,] graph)
     {
         // geprinte array is inverted...?
-        for(int i = 0; i < graph.GetLength(0); i++)
+        int graphHeight = graph.GetLength(0);
+        int graphWidth = graph.GetLength(1);
+
+        Console.SetWindowSize(graphWidth * 6, graphHeight * 6);
+
+        for (int i = 0; i < graphHeight; i++)
         {
-            for(int j = 0; j < graph.GetLength(1); j++)
+            for(int j = 0; j < graphWidth; j++)
             {
                 // left
-                Console.SetCursorPosition(i*4 + 1, j*4 + 4);
+                Console.SetCursorPosition(j*4 + 1, i*4 + 4);
                 System.Console.Write(graph[i,j].Left ? "<" : " ");
-                System.Console.Write("- ");
+                System.Console.Write("-");
                 
                 // value
+                Console.SetCursorPosition(j*4 + 4, i*4 + 4);
+                Console.ForegroundColor = ConsoleColor.Green;
                 System.Console.Write(graph[i, j].Value); 
+                Console.ForegroundColor = ConsoleColor.White;
                 
                 // right
-                System.Console.Write(" -");
+                Console.SetCursorPosition(j*4 + 6, i*4 + 4);
+                System.Console.Write("-");
                 if(graph[i,j].Right) System.Console.Write(">");
 
                 // up
-                Console.SetCursorPosition(i*4 + 4, j*4 + 1);
+                Console.SetCursorPosition(j*4 + 4, i*4 + 1);
                 if(graph[i,j].Up) System.Console.Write("^");
-                Console.SetCursorPosition(i*4 + 4, j*4 + 2);
+                Console.SetCursorPosition(j*4 + 4, i*4 + 2);
                 System.Console.Write("|");
 
                 // down
-                Console.SetCursorPosition(i*4 + 4, j*4 + 6);
+                Console.SetCursorPosition(j*4 + 4, i*4 + 6);
                 System.Console.Write("|");
-                Console.SetCursorPosition(i*4 + 4, j*4 + 7);
+                Console.SetCursorPosition(j*4 + 4, i*4 + 7);
                 if(graph[i,j].Down) System.Console.Write("v");
             }
         }
