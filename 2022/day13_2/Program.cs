9233be7d11b9b4aedb2584b5598feac82092c9b7
= <=>
2024-04-14 19:33:19 +0200

    temp

--- 2022/day13_2/Program.cs
+++ 2022/day13_2/Program.cs
@@ -1,29 +1,46 @@
-namespace day13_2;
+﻿namespace day13_2;
 
 class Program
 {
     static void Main(string[] args)
     {
         string[] input = File.ReadAllLines(args[0]);
-        List<int> correctIndices = new();
 
-        for (int i = 0; i < input.Count(); i += 3)
-        {
-            string left = input[i];
-            string right = input[i + 1];
+        // // part 1
+        // List<int> correctIndices = new();
 
-            bool? order = IsInOrder(left, right);
-            if (order == null)
-                throw new Exception("left and right are identical");
+        // for (int i = 0; i < input.Count(); i += 3)
+        // {
+        //     string left = input[i];
+        //     string right = input[i + 1];
 
-            if (order ?? false)
-            {
-                correctIndices.Add((i / 3) + 1); // 1-based
-            }
-        }
+        //     bool? order = IsInOrder(left, right);
+        //     if (order == null)
+        //         throw new Exception("left and right are identical");
 
-        System.Console.WriteLine($"Correct indices: {String.Join(',', correctIndices)}");
-        System.Console.WriteLine($"Sum of correct indices: {correctIndices.Sum()}");
+        //     if (order ?? false)
+        //     {
+        //         correctIndices.Add((i / 3) + 1); // 1-based
+        //     }
+        // }
+
+        // System.Console.WriteLine($"Correct indices: {String.Join(',', correctIndices)}");
+        // System.Console.WriteLine($"Sum of correct indices: {correctIndices.Sum()}");
+
+
+        // part 2
+        List<string> inputToSort = input
+            .Where(s => !string.IsNullOrEmpty(s))
+            .ToList();
+
+        inputToSort.Add("[[2]]");
+        inputToSort.Add("[[6]]");
+
+        LinkedList<string> llInputToSort = new LinkedList<string>(inputToSort);
+
+        QuickSort(llInputToSort);
+
+
     }
 
     private static bool? IsInOrder(string left, string right)
@@ -144,5 +161,44 @@ class Program
         }
 
         return returnvalue;
+    }
+
+    private static void QuickSort(LinkedList<string> input)
+    {
+        LinkedListNode<string> pivotNode = input.Last;
+
+        LinkedListNode<string> lastMovedNode = input.First;
+        for (LinkedListNode<string> currentNode = input.First; currentNode != pivotNode; currentNode = currentNode.Next)
+        {
+            if (IsInOrder(currentNode.Value, pivotNode.Value) ?? false)
+            {
+                if (lastMovedNode != currentNode)
+                {
+                    input.Remove(currentNode);
+                    input.AddAfter(lastMovedNode, currentNode);
+                    lastMovedNode = currentNode;
+                }
+            }
+        }
+
+        input.Remove(pivotNode);
+        input.AddAfter(lastMovedNode, pivotNode);
+
+        //todo selectie van begin tot pivotnode maken en dit recursive calle
+        // selectie van 1 voorbij pivot tot einde maken en dit recursive callen
+        // probleem: subselectie van linkedlist maken zonder een nieuwe linkelist te maken, want we hetzelfde object gebruiken (by ref)
+        QuickSort(input.);
+    }
+
+    private static int FindIndex(LinkedList<string> list, LinkedListNode<string> node)
+    {
+        int c = 1;
+
+        for (LinkedListNode<string> current = list.First; current != node; current = current.Next)
+        {
+            c++;
+        }
+
+        return c;
     }
 }
