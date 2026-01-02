0f74c18c3f021d56ad721340f99456a0744d01ac
Jason <jasondeveen@live.be>
2026-01-02 18:27:58 +0100

    day6 p1 & p2 rewrite (nachtmerrie)

--- 2025/day6/src/main.rs
+++ 2025/day6/src/main.rs
@@ -2,33 +2,96 @@ use std::{env::args, fs, time::SystemTime};
 
 static PART2: bool = true;
 
+#[derive(Debug)]
+struct Input<'a> {
+    lines: Vec<&'a str>,
+    current_width: usize,
+}
+
+impl<'a> Input<'a> {
+    fn build(lines: Vec<&'a str>) -> Result<Input<'a>, String> {
+        let mut res = Input {
+            lines,
+            current_width: 0,
+        };
+
+        res.update_current_width()?;
+
+        Ok(res)
+    }
+
+    fn update_current_width(&mut self) -> Result<(), String> {
+        let operand_line = self.lines.last().expect("No lines!");
+
+        // None = end of line
+        let word_width = operand_line
+            .chars()
+            .skip(2)
+            .take_while(|c| c.is_whitespace())
+            .count();
+
+        if word_width == 0 {
+            return Err(String::from("End of line reached"));
+        }
+
+        self.current_width = word_width;
+
+        Ok(())
+    }
+
+    fn current(&self) -> Vec<&'a str> {
+        let mut current_values = Vec::new();
+
+        for line in &self.lines {
+            current_values.push(&line[1..self.current_width + 1]);
+        }
+
+        current_values
+    }
+
+    fn advance(&mut self) -> Result<(), String> {
+        for line in &mut self.lines {
+            *line = &line.split_at(self.current_width).1[1..];
+        }
+
+        self.update_current_width()?;
+
+        Ok(())
+    }
+}
+
 fn main() {
     let now = SystemTime::now();
     let args: Vec<String> = args().collect();
-    let raw_input = fs::read_to_string(&args[1]).expect("Input not found! {&args[1]}");
+    let mut raw_input = fs::read_to_string(&args[1]).expect("Input not found! {&args[1]}");
 
-    let mut line_iterators = parse_input(&raw_input);
-    let number_of_lines = line_iterators.len();
+    raw_input = raw_input.replace("\r\n", " \r\n ");
+    raw_input = format!(" {raw_input}");
+    raw_input = raw_input
+        .chars()
+        .rev()
+        .collect::<String>()
+        .replacen("\n", "\n ", 1)
+        .chars()
+        .rev()
+        .collect();
 
-    let mut current_values = vec![""; number_of_lines];
+    let mut input = Input::build(raw_input.lines().collect()).expect("Failed to build input");
 
     let mut total = 0;
-    'outer: loop {
-        let mut i = 0;
-        for iterator in &mut line_iterators {
-            let temp = iterator.next();
-            match temp {
-                Some(val) => current_values[i] = val,
-                None => break 'outer,
-            }
-
-            i += 1;
-        }
+    loop {
+        let current_values = input.current();
 
         total += calculate(
-            &current_values[..number_of_lines - 1],
-            &current_values[number_of_lines - 1],
-        )
+            &current_values[..current_values.len() - 1],
+            &current_values[current_values.len() - 1],
+        );
+
+        let adv = input.advance();
+        match adv {
+            Ok(_) => (),
+            Err(_) => break,
+        }
     }
 
     println!("{:?}", now.elapsed().unwrap());
@@ -37,7 +100,7 @@ fn main() {
 
 fn calculate(operands: &[&str], operator: &str) -> u64 {
     let operation: fn(u64, u64) -> u64;
-    operation = match operator {
+    operation = match operator.trim() {
         "*" => |a, b| a * b,
         "+" => |a, b| a + b,
         _ => panic!("Unknown operator! {operator}"),
@@ -87,15 +150,43 @@ fn get_wacky_operands(operands: &[&str]) -> Vec<String> {
     wacky_values
 }
 
-fn parse_input<'a>(raw_input: &'a String) -> Vec<impl Iterator<Item = &'a str>> {
-    let mut iterators = Vec::new();
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    for line in raw_input.lines() {
-        iterators.push(line.split_whitespace());
-        // PROBLEEM: het splitten op whitespace verliest de info van hoe de cijfers onder elkaar
-        // staan. we zouden dus moeten de lijnen splitten zodat elke kolom even breed is, en dan
-        // moeten we later niet meer kijken voor te padden
+    #[test]
+    fn test_input() {
+        let test_cases = vec![(
+            Input::build(vec![
+                " 123 328  51 64  ",
+                "  45 64  387 23  ",
+                "   6 98  215 314 ",
+                " *   +   *   +   ",
+            ])
+            .expect("building Input failed"),
+            [
+                ["123", " 45", "  6", "*  "],
+                ["328", "64 ", "98 ", "+  "],
+                [" 51", "387", "215", "*  "],
+                ["64 ", "23 ", "314", "+  "],
+            ],
+        )];
+
+        for (mut input, expected_results) in test_cases {
+            for expected_result in expected_results {
+                {
+                    let result = input.current();
+                    assert_eq!(result.len(), expected_result.len());
+                    for i in 0..expected_result.len() {
+                        assert_eq!(expected_result[i], result[i]);
+                    }
+                }
+                let adv = input.advance();
+                match adv {
+                    Ok(_) => (),
+                    Err(_) => continue,
+                }
+            }
+        }
     }
-
-    iterators
 }
